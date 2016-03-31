2524b8e78ca08992e36058d3c2547f5ec8ae60bb
BKhurshid <josephsun4@gmail.com>
2016-03-30 18:59:42 -0700

    created getInitalState Method

--- App.js
+++ App.js
@@ -5,5 +5,29 @@ import CurrentLocation from './CurrentLocation';
 import LocationList from './LocationList';
 
 class App extends React.Component {
+  getInitialState() {
+    // Extract the favorite locations from local storage
+    let favorites = [];
 
+    if (localStorage.favorites) {
+      favorites = JSON.parse(localStorage.favorites);
+    }
+
+    // Paris will be the center
+
+    return {
+      favorites: favorites,
+      currentAddress: 'Paris, France',
+      mapCoordinates: {
+        lat: 48.856614,
+        lng: 2.3522219
+      }
+    };
+  },
+
+  toggleFavorite(address) {
+    if(this.isAddressInFavorites(address)) {
+      
+    }
+  }
 }
