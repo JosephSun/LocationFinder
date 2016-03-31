6a55856b4951744e8a3945f21f0e0872cd79cd0f
BKhurshid <josephsun4@gmail.com>
2016-03-30 18:55:54 -0700

    created app class

--- App.js
+++ App.js
@@ -3,3 +3,7 @@ import Search from './Search';
 import Map from './Map';
 import CurrentLocation from './CurrentLocation';
 import LocationList from './LocationList';
+
+class App extends React.Component {
+
+}
